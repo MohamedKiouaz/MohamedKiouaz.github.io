5b02e939e868ef4ae0cb121d578aa9e6cfc725ad
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2024-10-08 21:05:10 +0200

    Remove dead code

--- clicker.js
+++
@@ -1,8 +0,0 @@
-let count = 0;
-const button = document.getElementById('clicker-button');
-const counter = document.getElementById('counter');
-
-button.addEventListener('click', () => {
-    count++;
-    counter.textContent = `Clics: ${count}`;
-});
